83fedb6e65b2f1f5dcee6ae209f706234f9c6f15
Chrystoph <chrystophs@gmail.com>
2019-08-26 14:43:16 -0500

    Updates

---
+++ precache-manifest.8f3d53a577a3f1c96c70a95325e5d638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9924b179e8a20fea63bed5c952a0dd0c",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "fd061d489d1ad0febfcc",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6466b8e33da0c538031d",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "fd061d489d1ad0febfcc",
+    "url": "/contact-manager/static/js/2.1934ab24.chunk.js"
+  },
+  {
+    "revision": "6466b8e33da0c538031d",
+    "url": "/contact-manager/static/js/main.7f896734.chunk.js"
+  },
+  {
+    "revision": "7dc9f66a123f20ca34e8",
+    "url": "/contact-manager/static/js/runtime~main.b8eec7fc.js"
+  }
+]);
